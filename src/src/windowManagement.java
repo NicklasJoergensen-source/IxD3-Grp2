27ef3046e791c175a3c2cb0456a865a054851aad
PowerSupply-sudo <nicklasvinther@proton.me>
2025-11-03 13:27:12 +0100

    Initial setup of the project

---
+++ src/src/windowManagement.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class windowManagement {
+    public static void main(String[] args){
+        JFrame frame = new JFrame();
+        frame.setSize(2556,1179);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Test Frame");
+        frame.setVisible(true);
+    }
+}
